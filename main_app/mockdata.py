d45ebb40166233c9aad0254d64a78d653d80c056
dfokashch <01174305@pw.edu.pl>
2024-01-09 17:11:13 +0100

    Added mockdata

---
+++ main_app/mockdata.py
@@ -0,0 +1,29 @@
+incorrect_too_much_products_list_product = {
+    "products":[
+		{"width": 2, "height": 1},
+ 	    {"width": 2, "height": 2},
+      	{"width": 2, "height": 3},
+	],
+    "store_width": 3,
+    "store_height": 3
+}
+
+incorrect_full_list_product = {
+    "products":[
+		{"width": 2, "height": 1},
+ 	    {"width": 2, "height": 2},
+      	{"width": 1, "height": 3},
+	],
+    "store_width": 3,
+    "store_height": 3
+}
+
+correct_list_products = {
+	"products":[
+		{"width": 2, "height": 1},
+ 	    {"width": 2, "height": 2},
+      	{"width": 1, "height": 1},
+	],
+    "store_width": 3,
+    "store_height": 3
+}
